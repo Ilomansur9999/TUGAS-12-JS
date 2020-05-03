f6e414bed4444c993dd35d6801929066b37a5eaf
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 16:38:44 +0700

    TUGAS 12 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas12.js
@@ -0,0 +1,26 @@
+function panggilanTugas(){
+	var buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+	console.log(buah);
+
+	buah.pop();
+	console.log(buah);
+
+	buah.shift();
+	return buah
+
+}
+
+console.log(panggilanTugas());
+
+
+
+// BERHASIL SETELAH COBA COBA. TAPI SAYA BELUM TERLALU PAHAM LOGICNYA console.log(buah2);
+// C:\Users\USER\Documents\JS\javascript>node tugas12.js
+// [ 'Pisang', 'Jeruk', 'Apel', 'Mangga' ]
+// [ 'Pisang', 'Jeruk', 'Apel' ]
+// [ 'Jeruk', 'Apel' ]
+
+
+
+
+
